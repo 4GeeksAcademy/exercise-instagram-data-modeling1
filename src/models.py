0368f854fb89add3eb373d912309dff609a756ef
Felipe Alvim <128483571+fpalvim@users.noreply.github.com>
2024-07-22 15:49:11 +0000

    update

--- src/models.py
+++ src/models.py
@@ -13,17 +13,59 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    nickname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Follower(Base):
+    __tablename__ = 'follower'
+    # id = Column(Integer, primary_key=True)
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.   
+    person_to_id = Column(Integer, ForeignKey('person.id'))
+    person_from_id = Column(Integer, ForeignKey('person.id'), primary_key=True)
+    
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(3000))
+    person_id = Column(Integer, ForeignKey('person.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    person = relationship(Person)
+    post = relationship(Post)
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    type = Column(String(100), nullable=False)
+    url = Column(String(200), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
 
     def to_dict(self):
         return {}
